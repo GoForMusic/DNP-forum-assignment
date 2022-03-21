af7c3ad8ee9530c7b730fe54d50dc07090ad4346
Adrian <militaruadriancrisitna@gmail.com>
2022-03-21 12:20:33 +0100

    add vote

--- Domain/Models/Vote.cs
+++ Domain/Models/Vote.cs
@@ -3,13 +3,11 @@
 public class Vote
 {
     public string Id { get; set; }
-    public short Value { get; set; }
     public User Voter { get; set; }
 
     public Vote()
     {
         Id= RandomIDGenerator.Generate(20);
-        Value = 0;
         Voter = new User();
     }
 }
